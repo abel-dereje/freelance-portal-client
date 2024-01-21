b97571346dc8a478ce41ef6654d9a550142111f5
fullstackdevelopmentforall <abel.dereje94@gmail.com>
2024-01-21 17:10:50 +0300

    Render the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import LoginForm from './Components/LoginForm/LoginForm';
+import RegisterForm from './Components/LoginForm/RegisterForm';
 
 function App() {
   return (
     <div>
       <LoginForm />
+      <RegisterForm />
     </div>
   );
 }
